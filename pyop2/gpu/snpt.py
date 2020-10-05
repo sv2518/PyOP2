dea0b8c4d925ac8fe4ea3ed14311be39c88b2ec7
Kaushik Kulkarni <kgk2@illinois.edu>
2020-10-04 19:45:18 -0500

    snpt: make the kernel compilable for all forms

--- pyop2/gpu/snpt.py
+++ pyop2/gpu/snpt.py
@@ -14,22 +14,20 @@ def snpt_transform(kernel, block_size):
     kernel = lp.assume(kernel, "start < end")
     kernel = lp.split_iname(kernel, "n", block_size, outer_tag="g.0", inner_tag="l.0")
 
-    # {{{ making consts as globals
+    # {{{ making consts as globals: necessary to make the strategy emit valid
+    # kernels for all forms
 
     args_to_make_global = []
-    pack_consts_to_globals = False
+    args_to_make_global = [tv.initializer.flatten()
+                           for tv in kernel.temporary_variables.values()
+                           if (tv.initializer is not None and tv.address_space == lp.AddressSpace.GLOBAL)]
 
-    if pack_consts_to_globals:
-        args_to_make_global = [tv.initializer.flatten()
-                               for tv in kernel.temporary_variables.values()
-                               if (tv.initializer is not None and tv.address_space == lp.AddressSpace.GLOBAL)]
+    new_temps = dict((tv.name, tv.copy(initializer=None))
+                     if (tv.initializer is not None and tv.address_space == lp.AddressSpace.GLOBAL)
+                     else (tv.name, tv)
+                     for tv in kernel.temporary_variables.values())
 
-        new_temps = dict((tv.name, tv.copy(initializer=None))
-                         if (tv.initializer is not None and tv.address_space == lp.AddressSpace.GLOBAL)
-                         else (tv.name, tv)
-                         for tv in kernel.temporary_variables.values())
-
-        kernel = kernel.copy(temporary_variables=new_temps)
+    kernel = kernel.copy(temporary_variables=new_temps)
 
     # }}}
 
